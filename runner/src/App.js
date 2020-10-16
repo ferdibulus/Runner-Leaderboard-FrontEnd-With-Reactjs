6a3e5b11e04f9f1fca945ed26a957fc5933eae81
ferdibulus <=>
2020-10-17 01:46:09 +0300

    name changed

--- runner/src/App.js
+++ runner/src/App.js
@@ -5,7 +5,7 @@ import User from './components/User';
 function App(){
   return (
     <div className="container">
-      <Navbar title="User sdfsdfsdf"/>
+      <Navbar title="User ferdi"/>
       <User/>
     </div>
   );
