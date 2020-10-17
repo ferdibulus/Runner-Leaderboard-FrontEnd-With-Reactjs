768ba3dfbd9ab0d5e31c43d7ffbd757c822ded41
ferdibulus <=>
2020-10-17 22:13:59 +0300

    navbar is created

--- runner/src/App.js
+++ runner/src/App.js
@@ -1,12 +1,22 @@
 // eslint-disable-next-line
 import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import User from './components/User';
+import {Navbar} from 'react-bootstrap'
+import {Nav} from 'react-bootstrap'
 function App(){
   return (
-    <div className="container">
-      <Navbar title="User ferdi"/>
-      <User/>
+    <div className='container'>
+     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Runner Leaderboard</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    </Nav>
+                    <Nav>
+                    <Nav.Link href="https://www.technocodee.com/">@created by ferdibulus</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </Navbar>
     </div>
   );
 }
--- runner/src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Navbar(props) {
-    return (
-        <div>
-            <h3>{props.title}</h3>
-        </div>
-    )
-}
